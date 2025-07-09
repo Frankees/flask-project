8cdc1bd140fde06f7dbb6e96cb986753c6a7da24
PEDRO OLIVEIRA FRANKE <pedro_franke@estudante.sesisenai.org.br>
2025-07-09 08:59:39 -0300

    save

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask
 from app import app
 
-#app = Flask(__name__)
-#@app.route('/')
-#def homepage():
-#    return 'Minha'
-
-if __name__=='__main__':
+if __name__ == ('__main__'):
     app.run(debug=True)
